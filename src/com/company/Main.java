faf429167e5bab2532073f604b0ab746bdac9171
bbarnefiher <bb222@pvsd.org>
2018-09-24 10:52:15 -0400

    xd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Scanner stringReader = new Scanner(System.in);
+        String userPhrase = stringReader.nextLine();
+        userphrase.
 
 
 
